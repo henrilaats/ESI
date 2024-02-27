741c853c77ac89f2504dc794fec3df7551b44e8e
henrilaats <henrilaats55@gmail.com>
2024-02-27 10:43:00 +0200

    initial project

---
+++ demo/src/main/java/com/example/demo/testRestAPI.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testRestAPI {
+    @GetMapping("/esi")
+    public String helloWorld() {
+        return "Welcome to the ESI course!";
+    }
+}
